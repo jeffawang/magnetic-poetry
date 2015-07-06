8b8ed1a991cb51f0ee704896bb92e9cd070976d1
Jeff Wang <jeffawang@gmail.com>
2015-07-06 03:16:07 -0700

    Add magnet XY boundaries, so they cannot leave the board

--- js/magnets.jsx
+++ js/magnets.jsx
@@ -7,8 +7,15 @@ d3.json("/data/words.json", function(err, data) {
 
 var Board = React.createClass({
     grabbedMouseMove: function(index, startCoords, event) {
-        this.state.wordList[index].x = startCoords.x + event.clientX - startCoords.clientX
-        this.state.wordList[index].y = startCoords.y + event.clientY - startCoords.clientY
+        var node = React.findDOMNode(this.refs[index])
+        var maxX = width - node.offsetWidth
+        var maxY = height - node.offsetHeight
+        var dragX = startCoords.x + event.clientX - startCoords.clientX
+        var dragY = startCoords.y + event.clientY - startCoords.clientY
+        var newX = Math.max(0, Math.min(maxX, dragX))
+        var newY = Math.max(0, Math.min(maxY, dragY))
+        this.state.wordList[index].x = newX
+        this.state.wordList[index].y = newY
         this.setState(this.state)
     },
     grabbedMouseDown: function(index, event) {
@@ -48,6 +55,11 @@ var Board = React.createClass({
         return wordListObject
     },
 
+    xyBound: function(index, dim, by) {
+        this.state.wordList[index][dim] += by
+        this.setState(this.state)
+    },
+
     render: function() {
         var cx = React.addons.classSet
         var myClasses = cx({board: true, grabbing: this.state.grabbing})
@@ -58,6 +70,7 @@ var Board = React.createClass({
                 grabbing={this.state.grabbing}
                 grabbedId={this.state.grabbedId}
                 grabbedMouseDown={this.grabbedMouseDown.bind(this, d.id)}
+                xyBound={this.xyBound}
                 />
         }, this)
         return <div id="board" className={myClasses} >
@@ -76,6 +89,13 @@ var Magnet = React.createClass({
                 top:    this.props.data.y + "px"
         }
         return newStyle
+    },
+    componentDidMount: function() {
+        var node = React.findDOMNode(this)
+        var xCorrect = this.props.data.x + node.offsetWidth - width
+        var yCorrect = this.props.data.y + node.offsetHeight - height
+        if ( xCorrect > 0 ) this.props.xyBound(this.props.data.id, "x", -xCorrect)
+        if ( yCorrect > 0 ) this.props.xyBound(this.props.data.id, "y", -yCorrect)
     },
     render: function() {
         var omg = {"left": "100px"}
